fcc05c5770c8a6c20157fe8787ee9b030342961c
Sam Kennerly <samkennerly@gmail.com>
2020-01-05 17:22:53 -0500

    Add Ergast database image

--- code/tools.py
+++ code/tools.py
@@ -13,8 +13,8 @@ from plot import Plot
 
 REPO = Path(__file__).resolve().parent.parent
 DATADIR = REPO / "data"
-ERGAST_API = DATADIR / "ergast/api"
-ERGAST_ZIP = DATADIR / "ergast/f1.zip"
+ERGAST_API = DATADIR / "cache"
+ERGAST_ZIP = DATADIR / "f1.zip"
 TABLEDIR = DATADIR / "tables"
 PLOTDIR = DATADIR / "plots"
 
