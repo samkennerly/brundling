3635679b181f9e908a992b64e26b974177ce596f
Sam Kennerly <samkennerly@gmail.com>
2020-01-12 16:16:48 -0500

    Update copyrights

--- code/ergast_api.py
+++ code/ergast_api.py
@@ -223,19 +223,16 @@ class ErgastAPI:
                     warn(f"Gave up after {retries} retries.")
                     raise err
 
-
-"""
-Copyright © 2019 Sam Kennerly
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
+# Copyright © 2020 Sam Kennerly
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
